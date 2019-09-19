59ed60d2e89bb0213bfc982577873345b055be9d
Alex <mr.ivlex@gmail.com>
2019-09-19 22:56:25 +0500

    MultiCam

--- my-app.js
+++ my-app.js
@@ -19,8 +19,11 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main');
 
-var ip = 'http://192.168.3.147:52034/';
+var ip = 'http://192.168.3.151:18581/';
 var pwd = 'Yum8p3SRdN295k2';/*1017882C7824WIP*/
+
+var ppContent;
+
 
 function OnPtzMouseUp()
 {
@@ -73,35 +76,49 @@ url+='&' + new Date().getTime() + Math.random();
 $.getScript(url);
 }*/
 
+function newPopUp()
+{
+  var a = document.getElementsByClassName('swiper-slide-active');
+  ip = a[0].firstChild.currentSrc.substring(0,a[0].firstChild.currentSrc.indexOf('snapshot'));
+
+  // Create dynamic Popup
+  var dynamicPopup = app.popup.create({
+    content: '<div class="popup">' +
+                '<div class="block">'+
+                  '<img style="-webkit-user-select: none;" src="'+ ip +'videostream.cgi?user=admin&amp;pwd=' + pwd +'" width="640" height="360">'+
+                  '<button class="col button button-fill color-blue" style="width:360px" onclick="OnPtzMouseUp()">Up</button>' +
+                  '<button class="col button button-fill color-blue" style="width:360px" onclick="OnPtzMouseLeft()">Left</button>' +
+                  '<button class="col button button-fill color-blue" style="width:360px" onclick="OnPtzMouseRight()">Right</button>' +
+                  '<button class="col button button-fill color-blue" style="width:360px" onclick="OnPtzMouseDown()">Down</button>' +
+                  '<p><a href="#" class="link popup-close">Close me</a></p>'+
+                '</div>'+
+              '</div>',
+    // Events
+    on: {
+      open: function (popup) {
+        console.log('Popup open');
+      },
+      opened: function (popup) {
+        console.log('Popup opened');
+      },
+      close: function (popup) {
+        console.log('Popup close');
+        dynamicPopup.destroy();
+      }
+    }
+  });
+
+  dynamicPopup.open();
+}
+
 var swiper = app.swiper.create('.swiper-container', {
     speed: 400,
-    spaceBetween: 100
+    spaceBetween: 100,
+    slidesPerView: 1,
+    slidesPerColumn: 1,
+    centeredSlides: true
 });
 
-// Create dynamic Popup
-var dynamicPopup = app.popup.create({
-  content: '<div class="popup">' +
-              '<div class="block">'+
-                //'<p>Popup created dynamically.</p>'+
-                //'<div name="ipcamdiv" width="640" height="360" id="ipcamdiv" align="center" src="http://192.168.100.10:32227/videostream.cgi?user=admin&pwd=1017882C7824WIP" style="width: 640px; height: 360px;"></div>' +
-                '<img style="-webkit-user-select: none;" src="'+ ip +'videostream.cgi?user=admin&amp;pwd=' + pwd +'" width="640" height="360">'+
-                '<button class="col button button-fill color-blue" style="width:360px" onclick="OnPtzMouseUp()">Up</button>' +
-                '<button class="col button button-fill color-blue" style="width:360px" onclick="OnPtzMouseLeft()">Left</button>' +
-                '<button class="col button button-fill color-blue" style="width:360px" onclick="OnPtzMouseRight()">Right</button>' +
-                '<button class="col button button-fill color-blue" style="width:360px" onclick="OnPtzMouseDown()">Down</button>' +
-                '<p><a href="#" class="link popup-close">Close me</a></p>'+
-              '</div>'+
-            '</div>',
-  // Events
-  on: {
-    open: function (popup) {
-      console.log('Popup open');
-    },
-    opened: function (popup) {
-      console.log('Popup opened');
-    },
-    close: function (popup) {
-  	  console.log('Popup close');
-  	}
-  }
-});
+swiper.removeAllSlides();
+swiper.addSlide(0,'<div class="swiper-slide"><img src="http://192.168.3.151:18581/snapshot.cgi?res=0&user=admin&pwd=Yum8p3SRdN295k2"/></div>');
+swiper.addSlide(1,'<div class="swiper-slide"><img src="http://192.168.3.147:55845/snapshot.cgi?res=0&user=admin&pwd=Yum8p3SRdN295k2"/></div>');
